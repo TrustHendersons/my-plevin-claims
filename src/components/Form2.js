f41423e9afdc4109280c7c60720835f2ff9d7029
2Owls <hello@2owls.co.uk>
2021-08-06 17:22:33 +0100

    commit

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -3,22 +3,14 @@ import React from "react";
 function Form2({ register, shouldDisplay }) {
   return (
     <div style={{ display: shouldDisplay ? "block" : "none" }}>
-      <form
-        name="contact"
-        method="post"
-        action="/thank-you/"  
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"           
-      >  
-      <input type="hidden" name="form-name" value="contact" />     
-        <br />
+
         <div className="form-group">
           <label>User name</label>
-          <input type="text" name="uname" {...register("uname", { required: true } )} />
+          <input type="text" name="firstName" {...register("firstName", { required: true } )} />
           <label>Email</label>
           <input type="email" name="email" {...register("email", { required: true } )} />
         </div>
-      </form>
+
     </div>
   );
 }
---
+++ src/pages/thank-you.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+
+
+const IndexPage = () => {
+
+  return (
+
+  <Layout>
+    <div class="container">
+      <h1>Start your Plevin Check now</h1>
+      <p>Complete the quick and easy process below to see if you have a Plevin claim - <strong>Free online check</strong></p>
+      <div>
+          <section className="section">
+              <h1>Thank you for your interest</h1>
+          </section>  
+      </div>
+    </div>
+  </Layout>
+)
+
+  }
+
+export default IndexPage
